fb006a3bc3042d0ef1756607ed3e9f816739c4af
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-21 19:50:23 +0000

    added container-fluid and adjusted sizes

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-       <div className="row">
-            <div className="jumbotron col-lg-8 col-sm-11 mx-auto my-3 bg-light rounded">
+       <div className="container-fluid">
+            <div className="jumbotron col-lg-7 col-sm-10 mx-auto my-3 p-3 bg-blueishgray rounded">
                 <h1 className="display-4">A Warm Welcome!</h1>
                 <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero minima animi dolorum rem eum placeat dolor. Adipisci similique dicta, ipsam molestiae blanditiis fugiat unde mollitia harum perspiciatis! Aperiam, dolores similique.</p>
                 <p className="lead">
                     <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
                 </p>
             </div>
-       </div>
+        </div>
     );
 };
 
