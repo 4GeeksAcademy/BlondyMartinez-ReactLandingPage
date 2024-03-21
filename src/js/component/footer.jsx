46f68357b7b12b590c9fda40069a04d88cdb40a2
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-21 19:50:59 +0000

    added footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="row p-5 bg-dark text-white text-center">
+            <p>Made with ❤️ by Blondy Martínez</p>
+        </div>
+    );
+};
+
+export default Footer;
